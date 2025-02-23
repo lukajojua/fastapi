dd15959dff0e9cad6f9e8b28f7d7f05b45223132
Tiko <tikotsakadadze2018@gmail.com>
2025-02-24 02:25:38 +0400

    added method for user passwords

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import bcrypt
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
@@ -8,6 +9,15 @@ class User(Base):
     email = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
     posts = relationship("Post", back_populates="owner")
+
+    def set_password(self, password):
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        self.hashed_password = hashed_password.decode('utf-8')
+
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.hashed_password.encode('utf-8'))
+
 
 class Post(Base):
     __tablename__ = 'posts'
